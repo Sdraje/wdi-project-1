aded9fc25f90d2dc2220b0e34a577c67f0d9ad8b
Sdraje <alfmarmil@gmail.com>
2016-08-22 09:39:43 +0100

    Broken mess

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // startGame
-
 // Bubble constructor
 // new Bubble(x, y, timer)
 
@@ -23,68 +22,71 @@
 
 // gameOver
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var game = game || {};
 
 $(document).ready(init);
 
+game.newGame = function newGame(){
+
+}
+
 function init(){
-  $pop                = $('body').on('click', '.a', popBubble);
-  game.$DivA          = $('.a');
+  game.$pop           = $('body').on('click', '.bubbles', game.popBubble);
+  game.$bubbles       = $('.bubbles');
   $main               = $('main');
   game.score          = 0;
+  game.lives          = 10;
+  $('#score').html("SCORE " + game.score)
+  $('#lives').html('LIVES ' + game.lives)
+  game.interval();
+};
 
-  game.changeInterval = function (){
-    (game.$DivA.length*2000) + 1000
-  }
-
-  $.each(game.$DivA, animateDivA);
-
+game.interval = function interval (){
   setInterval(function(){
-    game.$DivA = $('.a');
-    if (game.$DivA.length > 9) return;
-
-    var posx    = (Math.random() * ($main.width() - 50)).toFixed();
-    var posy    = (Math.random() * ($main.height() - 50)).toFixed();
-    $newDiv     = $('<div />', {"class": 'a', 'value': 10});
-    game.$DivA  = $('.a');
-    $newDiv.css({'left':posx+'px', 'top':posy+'px'}).appendTo($main);
-    $newDiv.html($newDiv.attr('value'))
-    // $pop        = game.$DivA.on('click', popBubble);
-    
-    $.each(game.$DivA, animateDivA);
+    if (game.$bubbles.length > 9) {return}
+    game.createBubble();
+    game.$bubbles  = $('.bubbles');
+    $.each(game.$bubbles, game.animateBubble);
   }, 1000);
 };
 
-function popBubble () {
-  console.log('this value', $(this).attr('value'))
+// Explode bubble
+game.popBubble = function popBubble () {
   game.score += parseInt($(this).attr('value'))
-  console.log('gamescore', game.score)
   $('#score').html("SCORE " + game.score)
+  var bubblePop = new Audio("sounds/bubblePop.mp3");
+  bubblePop.play();
   $(this).remove();
 }
 
 /*
+ * Animating the movement of the div
+ */
+ game.animateBubble = function animateBubble(i, array){
+  $(array).attr('value', $(array).attr('value')-1);
+  $(array).html($(array).attr('value'));
+
+  if ($(array).attr('value') <= 0){
+    var bubblePop = new Audio("sounds/bubblePop.mp3");
+    bubblePop.play();
+    $(array).remove()
+    // game.$bubbles.splice(i, 1) --- need to remove bubbles from array (loop still counts them)
+    game.livesCheck();
+    return;
+  };
+
+  var newq = game.makeNewPosition();
+  var oldq = $(array).offset();
+  var speed = game.calcSpeed([oldq.top, oldq.left], newq);
+  $(array).animate({ top: newq[0], left: newq[1] }, speed, function(){
+    game.animateBubble(i, array);        
+  });
+};
+
+/*
  * Create a new start position to place a bubble
  */
-function makeNewPosition(){
+ game.makeNewPosition = function makeNewPosition(){
   var h  = $('main').height() - 50;
   var w  = $('main').width() - 50;
   var nh = Math.floor(Math.random() * h);
@@ -93,31 +95,7 @@ function makeNewPosition(){
   return [nh,nw];        
 }
 
-/*
- * Animating the movement of the div
- */
-function animateDivA(i, array){
-  // Explode bubble
-  setInterval(function(){
-    $(array).attr('value', $(array).attr('value')-1);
-    $(array).html($(array).attr('value'));
-
-    if ($(array).attr('value') <= 0){
-      $(array).remove(); 
-    }
-  }, 2000)
-  
-  // setTimeout (function(){$(array).remove()}, 10000)--------------working
-  var newq = makeNewPosition();
-  var oldq = $(array).offset();
-  var speed = calcSpeed([oldq.top, oldq.left], newq);
-
-  $(array).animate({ top: newq[0], left: newq[1] }, speed, function(){
-    animateDivA(i, array);        
-  });
-};
-
-function calcSpeed(prev, next) {
+game.calcSpeed = function calcSpeed(prev, next) {
   var x             = Math.abs(prev[1] - next[1]);
   var y             = Math.abs(prev[0] - next[0]);
   var greatest      = x > y ? x : y;
@@ -125,4 +103,28 @@ function calcSpeed(prev, next) {
   var speed         = Math.ceil(greatest/speedModifier);
   return speed;
 
+}
+
+game.createBubble = function (){
+  var newPosition = game.makeNewPosition();
+  $newBubble      = $('<button />', {"class": 'bubbles', 'value': 11});
+  $newBubble.css({'top':newPosition[0]+'px', 'left':newPosition[1]+'px'}).appendTo($('main'));
+  $newBubble.html($newBubble.attr('value'))
+}
+
+game.livesCheck = function livesCheck () {
+  game.lives--
+  $('#lives').html('LIVES ' + game.lives)
+  if (game.lives === 0) {game.over()}
+}
+
+game.over = function () {
+  var person = prompt ('Game over! Please enter your name:')
+  $('#high-scores').append('<li>' + person + " " + game.score + '</li>')
+  $('main').empty();
+  // game.$bubbles.empty(); -------------------------- preventing new bubbles from being created
+  game.score          = 0;
+  game.lives          = 10;
+  $('#score').html("SCORE " + game.score)
+  $('#lives').html('LIVES ' + game.lives)
 }
